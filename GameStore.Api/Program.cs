05935bd1ec6291caa37ee2a2046d37193f6a0f68
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-07 20:31:26 +0200

    feat: add mock games data

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,6 +1,17 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = new List<GameDto>
+{
+    new GameDto(1, "The Legend of Zelda: Breath of the Wild", "Action-adventure", 59.99, new DateOnly(2017, 3, 3)),
+    new GameDto(2, "Super Mario Odyssey", "Platform", 59.99, new DateOnly(2017, 10, 27)),
+    new GameDto(3, "Red Dead Redemption 2", "Action-adventure", 59.99, new DateOnly(2018, 10, 26)),
+    new GameDto(4, "The Witcher 3: Wild Hunt", "Action RPG", 39.99, new DateOnly(2015, 5, 19)),
+    new GameDto(5, "Minecraft", "Sandbox", 26.95, new DateOnly(2011, 11, 18))
+};
+
+app.MapGet("/games", () => "Hello World!");
 
 app.Run();
