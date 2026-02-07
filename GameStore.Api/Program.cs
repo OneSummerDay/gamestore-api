089beb7d8202fef0eddde6ff3e609cd9f3426eab
OneSummerDay <artem.kraievyi@gmail.com>
2026-02-07 22:09:31 +0200

    feat: add PUT endpoint to update game

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -27,4 +27,17 @@ app.MapPost("/games", (CreateGameDto newGame) =>
     return Results.Created($"/games/{game.Id}", game);
 });
 
+app.MapPut("/games/{id}", (int id, UpdateGameDto updatedGame) =>
+{
+    var game = games.FirstOrDefault(game => game.Id == id);
+    if (game is null)
+    {
+        return Results.NotFound();
+    }
+
+    var updatedGameDto = new GameDto(id, updatedGame.Name, updatedGame.Genre, updatedGame.Price, updatedGame.ReleaseDate);
+    games[games.IndexOf(game)] = updatedGameDto;
+    return Results.Ok(updatedGameDto);
+});
+
 app.Run();
